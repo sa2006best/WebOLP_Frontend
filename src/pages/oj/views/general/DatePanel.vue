049bd3bc5560b767b6b4d89876267519ba505295
123123@@gh <sa2006best@gmail.com>
2021-03-13 21:32:39 +0700

    Init

---
+++ src/pages/oj/views/general/DatePanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <Panel shadow :padding-bottom="10" style="border-radius: 10px">
+    <template v-slot:title>
+      <div class="text-center" style="width: 100%">
+        <span style="font-size: small">Rủ bạn bè vào cày rank thôi nào!</span>
+      </div>
+      <div class="text-center" style="width: 100%">
+        <Icon color="#fdee2d" style="font-size: medium" type="ios-star" /><Icon color="#fdee2d" type="ios-star" /><Icon color="#fdee2d" style="font-size: medium" type="ios-star" />
+      </div>
+    </template>
+    <h1 style="width: 100%" class="text-center">{{dayInWeek}}</h1>
+    <h1 style="width: 100%" class="text-center">{{todayInDate}}</h1>
+    <div class="ivu-card-head">
+      <div class="panel-title text-center">
+        <span style="font-size: small">- No pain, no gain -</span>
+      </div>
+    </div>
+  </Panel>
+</template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    name: 'DatePanel',
+    components: {},
+    data () {
+      return {
+        todayInDate: moment().format('DD/MM/YYYY'),
+        dayInWeek: moment().format('dddd'),
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      init () {
+      }
+    },
+    computed: {
+    }
+  }
+</script>
+
+<style scoped>
+  .text-center {
+    text-align: center;
+  }
+</style>
